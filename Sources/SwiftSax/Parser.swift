292e5174d9eceee4d2f22e494dbca01b6b366e51
Jan Krukowski <jan.krukowski@concur.com>
2020-03-18 18:22:06 +0100

    removed unused

--- Sources/SwiftSax/Parser.swift
+++ Sources/SwiftSax/Parser.swift
@@ -7,9 +7,6 @@
 
 import Clibxml2
 import Foundation
-import Logging
-
-let logger = Logger(label: "SwiftSax.Parser")
 
 open class Parser {
     open var options: Parser.Option
@@ -19,14 +16,12 @@ open class Parser {
         self.options = options
         let inputPointer = try data.withUnsafeBytes { (input: UnsafeRawBufferPointer) -> UnsafePointer<CChar> in
             guard let inputPointer = input.bindMemory(to: CChar.self).baseAddress else {
-                logger.error("Couldn't find input pointer")
                 throw ParserError.data
             }
             return inputPointer
         }
         let parseOptions = CInt(options.rawValue)
         guard let parserContext = htmlReadMemory(inputPointer, Int32(data.count), "", nil, parseOptions) else {
-            logger.error("Couldn't create parser context")
             throw ParserError.context
         }
         self.parserContext = parserContext
@@ -34,12 +29,10 @@ open class Parser {
 
     open func find(path: String) throws -> [Node] {
         guard let xpathContext = xmlXPathNewContext(parserContext) else {
-            logger.error("Couldn't create xPath context")
             throw ParserError.context
         }
         defer { xmlXPathFreeContext(xpathContext) }
         guard let xpath = xmlXPathEvalExpression(path, xpathContext) else {
-            logger.error("Couldn't evaluate xPath expression")
             throw ParserError.xpath
         }
         defer { xmlXPathFreeObject(xpath) }
